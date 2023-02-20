889b91772bb69f83a360f3c8e2f41e41da27d37e
Ivan Cao-Berg <icaoberg@psc.edu>
2023-02-20 04:46:48 -0500

    Updated histogram script

--- histogram.sh
+++ histogram.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-module load lowcharts
+TYPE=$1
+FILENAME=$2
 
-FILENAME=$1
-cat $FILENAME | cut -d$'\t' -f2 | sort | lowcharts common-terms
+if [[ "$TYPE" == "extension" ]]; then
+	cat $FILENAME | cut -d$'\t' -f2 | sort | lowcharts common-terms
+fi
+
+if [[ "$TYPE" == "filetype" ]]; then
+	cat $FILENAME | cut -d$'\t' -f4 | sort | lowcharts common-terms
+fi
+
+if [[ "$TYPE" == "mime-type" ]]; then
+	cat $FILENAME | cut -d$'\t' -f7 | sort | lowcharts common-terms
+fi
