3a49d7fbdcec53767e08e5d39176eb0c6a9ddeb9
Ivan Cao-Berg <icaoberg@psc.edu>
2023-03-10 18:49:25 -0500

    Added test script

--- hubmapinventory/__init__.py
+++ hubmapinventory/__init__.py
@@ -2,8 +2,8 @@
 hubmapinventory
 """
 
-__version__ = '2023.02' # year 2023 2nd quarter
-__author__ = 'Ivan Cao-Berg'
-__credits__ = 'HuBMAP IEC Team'
+__version__ = "2023.02"  # year 2023 2nd quarter
+__author__ = "Ivan Cao-Berg"
+__credits__ = "HuBMAP IEC Team"
 
 from .inventory import *
