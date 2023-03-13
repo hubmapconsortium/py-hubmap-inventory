a0b78a6e594d1cc8d5c85d90317acabd588a6045
icaoberg <icaoberg@andrew.cmu.edu>
2023-03-13 07:41:26 -0400

    Added helpful scripts

---
+++ codex_only.py
@@ -0,0 +1,29 @@
+import hubmapbags
+
+import hubmapinventory
+
+token = "this-is-my-token"
+ncores = 25
+
+assay_types = hubmapbags.apis.get_assay_types(token=token)
+assay_types.remove("CODEX")
+assay_types.remove("codex_cytokit")
+assay_types.remove("codex_cytokit_v1")
+
+compute_uuids = False
+for assay_type in assay_types:
+    print(assay_type)
+    datasets = hubmapbags.apis.get_hubmap_ids(assay_type, token=token)
+
+    for dataset in datasets:
+        if (
+            not dataset["is_protected"]
+            and dataset["is_primary"]
+            and dataset["status"] == "Published"
+        ):
+            df = hubmapinventory.inventory.create(
+                dataset["hubmap_id"],
+                token=token,
+                ncores=ncores,
+                compute_uuids=compute_uuids,
+            )
