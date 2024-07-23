bb7c3d338e190d7167f60f389ef565405801015b
Ivan Cao-Berg <icaoberg@psc.edu>
2024-07-22 22:51:51 -0400

    Updated scripts

---
+++ examples/codex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -n 30
+#SBATCH -p batch
+#SBATCH --mem=128G
+#SBATCH -o "codex-job-%j.out"
+#SBATCH -e "codex-job-%j.out"
+
+module load anaconda
+. "/hive/users/hive/anaconda3/etc/profile.d/conda.sh"
+conda activate /hive/users/hive/icaoberg/py-hubmapbags/env
+
+export TOKEN=""
+python ./codex.py
---
+++ examples/derived.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -n 30
+#SBATCH -p batch
+#SBATCH --mem=128G
+
+module load anaconda
+. "/hive/users/hive/anaconda3/etc/profile.d/conda.sh"
+conda activate /hive/users/hive/icaoberg/py-hubmapbags/env
+
+export TOKEN=""
+python ./derived.py
